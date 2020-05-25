878d0b59c8133a76f25e9562f0d788a8b643d90e
Vaibhav Agrawal <vaibhavagrawal.ece09@gmail.com>
2020-05-25 23:32:46 +0100

    adding i18n services to web skelton

--- src/test/java/com/devopslearning/DevopslearningApplicationTests.java
+++ src/test/java/com/devopslearning/DevopslearningApplicationTests.java
@@ -1,13 +1,28 @@
 package com.devopslearning;
 
+import com.devopslearning.web.i18n.I18NService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.util.Assert;
+
 
 @SpringBootTest
+@WebAppConfiguration
 class DevopslearningApplicationTests {
 
+    @Autowired
+    private I18NService i18NService;
+
     @Test
-    void contextLoads() {
+    public void testMessgaeI18n() throws Exception{
+        String expectedResult =  "Bootstrap Starter Template";
+        String messageId = "index.main.callout";
+        String actual_message = i18NService.getMessage(messageId);
+        //Assertions.assertEquals(expectedResult, actual_message, "The expected message do not matches with actual messg");
+        Assertions.assertEquals(expectedResult, actual_message, "The expected message do not matches with actual messg");
     }
 
 }
