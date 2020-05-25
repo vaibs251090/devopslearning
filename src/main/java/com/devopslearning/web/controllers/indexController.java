fc3025cb3595817df910197c64d75348672ca02c
Vaibhav Agrawal <vaibhavagrawal.ece09@gmail.com>
2020-05-25 19:06:13 +0100

    added placeholder index

---
+++ src/main/java/com/devopslearning/web/controllers/indexController.java
@@ -0,0 +1,13 @@
+package com.devopslearning.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+@RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+
+}
